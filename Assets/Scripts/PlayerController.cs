ba6bcf6fa598f7f22e837a704ebb28fab78ddd28
DimBlessing <47944912+DimBlessing@users.noreply.github.com>
2024-06-18 15:56:28 +0900

    Player Control / TileMap / Camera Settings

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,26 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    public Transform playerRoot;
     public Rigidbody2D rb;
-    public float speed = 5f;
-    private float x;
-    private float y;
+    public Animator playerAnimator;
+    //public SpriteRenderer spriteRenderer;
+    public float speed = 3f;
+    public Vector2 inputVec;
+
+    private Vector3 currentRotation;
     void Start()
     {
-        rb = gameObject.GetComponent<Rigidbody2D>();
+        playerRoot = gameObject.transform.GetChild(0).GetComponent<Transform>();
+        rb = GetComponent<Rigidbody2D>();
+        //spriteRenderer = GetComponent<SpriteRenderer>();
+        playerAnimator = gameObject.transform.GetChild(0).GetComponent<Animator>();
     }
 
-
-    void Update()
-    {
-        x = Input.GetAxis("Horizontal");
-        y = Input.GetAxis("Vertical");
+    void OnMove(InputValue value){
+        inputVec = value.Get<Vector2>();
     }
 
     void FixedUpdate(){
-        rb.velocity = new Vector2(x, y) * speed;
+        Vector2 nextVec = inputVec.normalized * speed * Time.fixedDeltaTime;
+        rb.MovePosition(rb.position + nextVec);
+    }
+    
+    void LateUpdate(){
+        playerAnimator.SetFloat("Speed", inputVec.magnitude);
+        currentRotation = playerRoot.localEulerAngles;
+        if(inputVec.x != 0){
+            //spriteRenderer.flipX = inputVec.x < 0;
+            if(inputVec.x > 0){
+                currentRotation.y = 180f;
+                playerRoot.localEulerAngles = currentRotation;
+            }
+            else{
+                currentRotation.y = 0f;
+                playerRoot.localEulerAngles = currentRotation;
+            }
+        }
     }
 }
